468a8558ad482df463159c71f8b300938ed90c1e
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-01-23 11:48:20 -0500

    fixed typo

--- projects/lavellt/dominion/dominion.c
+++ projects/lavellt/dominion/dominion.c
@@ -643,7 +643,8 @@ int getCost(int cardNumber)
   return -1;
 }
 
-int adventurer(int currentPlayer, gameState *state, int cardDrawn, int temphand, int z) {
+int adventurer(int currentPlayer, struct gameState *state, int cardDrawn, int temphand, int z) {
+	int drawntreasure = 0;
 	while (drawntreasure<2) {
 		if (state->deckCount[currentPlayer] <1) {//if the deck is empty we need to shuffle discard and add to deck
 			shuffle(currentPlayer, state);
@@ -677,7 +678,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
-  int drawntreasure=0;
+  /*int drawntreasure=0;*/
   int cardDrawn;
   int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
