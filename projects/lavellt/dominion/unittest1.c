21ecab1787c925587838d3420718a9a7dce47328
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-01-30 13:28:21 -0500

    ut1 almost done. ut2 started

--- projects/lavellt/dominion/unittest1.c
+++ projects/lavellt/dominion/unittest1.c
@@ -1,3 +1,5 @@
+// Tucker Lavell
+// unit tests 1: playCard
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include "rngs.h"
@@ -6,28 +8,25 @@
 #include <stdlib.h>
 
 int phaseTest();
+int playCard_actionsTest();
+int playCard_checkActionTest_adventurer();
+int playCard_checkActionTest_treasure_map();
+int playCard_cardEffectTest();
 //#define SUCCESS "TEST  SUCCESSFULLY COMPLETED"
 //#define FAILURE "TEST FAILED"
 
 
 int main() {
+	int passed = 0;
+	int numTests = 5;
 
-	// play card
-	//struct gameState* gS = newGame();
-	//
-	//int expected = -1;
-	//int actual = playCard(0, 0, 0, 0, gS);
+	passed += playCard_phaseTest();
+	passed += playCard_actionsTest();
+	passed += playCard_checkActionTest_adventurer();
+	passed += playCard_checkActionTest_treasure_map();
+	//passed += playCard_cardEffectTest();
 
-	//if (actual == expected) {
-	//	std::cout << "TEST  SUCCESSFULLY COMPLETED" << endl;
-	//}
-	//else {
-	//	std::cout << "TEST FAILED" << endl;
-	//}
-	playCard_phaseTest();
-	playCard_actionsTest();
-	playCard_checkActionTest_adventurer();
-	playCard_checkActionTest_treasure_map();
+	printf("%d/%d playCard Tests Passed.", passed, numTests);
 }
 
 int playCard_phaseTest() {
@@ -79,7 +78,8 @@ int playCard_checkActionTest_adventurer() {
 	gS->phase = 0;
 	gS->numActions = 1;
 	gS->whoseTurn = 0;
-
+	// setting first card of P1s hand to any card lower in the enum than adv
+	gS->hand[0][0] = curse; // 0
 
 	int expected = -1;
 	int actual = playCard(0, 0, 0, 0, gS);
@@ -95,9 +95,55 @@ int playCard_checkActionTest_adventurer() {
 	}
 
 	delete gS;
-
 }
 
-//int playCard_checkActionTest_treasure_map() {
+int playCard_checkActionTest_treasure_map() {
+	struct gameState* gS = newGame();
+
+	gS->phase = 0;
+	gS->numActions = 1;
+	gS->whoseTurn = 0;
+	// setting first card of P1s hand to any card higher in the enum than TM
+	gS->hand[0][0] = 27; // beyond the current set of cards
+
+	int expected = -1;
+	int actual = playCard(0, 0, 0, 0, gS);
+
+
+	if (actual == expected) {
+		printf("TEST  SUCCESSFULLY COMPLETED\n");
+		return 1;
+	}
+	else {
+		printf("TEST FAILED\n");
+		return 0;
+	}
+
+	delete gS;
+}
+
+// incompelete
+//int playCard_cardEffectTest() {
+//	struct gameState* gS = newGame();
 //
+//	gS->phase = 0;
+//	gS->numActions = 1;
+//	gS->whoseTurn = 0;
+//	// setting first card of P1s hand to any card higher in the enum than TM
+//	gS->hand[0][0] = 27; // beyond the current set of cards
+//
+//	int expected = -1;
+//	int actual = playCard(0, 0, 0, 0, gS);
+//
+//
+//	if (actual == expected) {
+//		printf("TEST  SUCCESSFULLY COMPLETED\n");
+//		return 1;
+//	}
+//	else {
+//		printf("TEST FAILED\n");
+//		return 0;
+//	}
+//
+//	delete gS;
 //}
---
+++ projects/lavellt/dominion/unittest2.c
@@ -0,0 +1,92 @@
+// Tucker Lavell
+// unit tests 2: isGameOver
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int isGameOver_isProvinceEmpty();
+int isGameOver_is3SupplyEmpty();
+int isGameOver_isGameNOTOver();
+
+int main() {
+	int passed = 0;
+	int numTests = 3;
+
+	passed += isGameOver_isProvinceEmpty();
+	passed += isGameOver_is3SupplyEmpty();
+	passed += isGameOver_isGameNOTOver();
+
+	printf("%d/%d isGameOver Tests Passed.", passed, numTests);
+}
+
+int isGameOver_isProvinceEmpty() {
+	struct gameState* gS = newGame();
+
+	gS->supplyCount[province] = 0;
+
+	int expected = 1;
+	int actual = isGameOver(gS);
+
+
+	if (actual == expected) {
+		printf("TEST  SUCCESSFULLY COMPLETED\n");
+		return 1;
+	}
+	else {
+		printf("TEST FAILED\n");
+		return 0;
+	}
+
+	delete gS;
+}
+
+int isGameOver_is3SupplyEmpty() {
+	struct gameState* gS = newGame();
+	//int k[10] = { adventurer, gardens, embargo, village, minion, mine, cutpurse,
+	//	sea_hag, tribute, smithy };
+	//initializeGame(2, k, 1, &gS);
+
+	gS->supplyCount[curse] = 0;
+	gS->supplyCount[silver] = 0;
+	gS->supplyCount[duchy] = 0;
+
+	int expected = 1;
+	int actual = isGameOver(gS);
+
+
+	if (actual == expected) {
+		printf("TEST  SUCCESSFULLY COMPLETED\n");
+		return 1;
+	}
+	else {
+		printf("TEST FAILED\n");
+		return 0;
+	}
+
+	delete gS;
+}
+
+int isGameOver_isGameNOTOver() {
+	struct gameState* gS = newGame();
+	int k[10] = { adventurer, gardens, embargo, village, minion, mine, cutpurse,
+		sea_hag, tribute, smithy };
+	initializeGame(2, k, 1, &gS);
+
+	int expected = 0;
+	int actual = isGameOver(gS);
+
+	// a brand new game shouldn't be ready to end
+	if (actual == expected) {
+		printf("TEST  SUCCESSFULLY COMPLETED\n");
+		return 1;
+	}
+	else {
+		printf("TEST FAILED\n");
+		return 0;
+	}
+
+	delete gS;
+}
