4efe94abccab728b4666e3a9b22c54d5b44f0418
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-02-01 13:45:18 -0500

    testing ct4 t2 original

--- projects/lavellt/dominion/cardtest4.c
+++ projects/lavellt/dominion/cardtest4.c
@@ -2,14 +2,14 @@
 // card test 4: treasure_map
 #include "unittests.h"
 
-int treasure_map_checkTrashCount();
+int treasure_map_checkNumCardsPlayed();
 
 
 int main() {
 	int passed = 0;
 	int numTests = 1;
 
-	passed += treasure_map_checkTrashCount();
+	passed += treasure_map_checkNumCardsPlayed();
 	//passed += playCard_actionsTest();
 	//passed += playCard_checkActionTest_adventurer();
 	//passed += playCard_checkActionTest_treasure_map();
@@ -18,7 +18,7 @@ int main() {
 	printf("%d/%d Treasure Map Card Tests Passed.\n", passed, numTests);
 }
 
-int treasure_map_checkTrashCount() {
+int treasure_map_checkNumCardsPlayed() {
 	struct gameState* gS = newGame();
 
 	// can't check which cards are trashed, but we can set the 
@@ -44,10 +44,38 @@ int treasure_map_checkTrashCount() {
 	return assertTrue(actual, expected);
 }
 
-//int treasure_map_checkTrash() {
-//
-//}
-//
+int treasure_map_checkHand() {
+	struct gameState* gS = newGame();
+
+	// can't check which cards are trashed, but we can set the 
+	// playedCardCount = 0, and if it stays at 0 when playing,
+	// treasure_map, then we can assume the treasure maps were discarded
+	int expected = 0;
+	int actual = 0;
+
+	gS->whoseTurn = 0;
+	gS->handCount[gS->whoseTurn] = 4;
+	gS->hand[gS->whoseTurn][0] = treasure_map;
+	gS->hand[gS->whoseTurn][1] = copper;
+	gS->hand[gS->whoseTurn][2] = copper;
+	gS->hand[gS->whoseTurn][3] = treasure_map;
+	gS->playedCardCount = 0;
+
+	cardEffect(treasure_map, 0, 0, 0, gS, 0, 0);
+
+	if (gS->whoseTurn == 0) {
+		int i;
+		actual++;
+		for (i = 0; i < gS->handCount[gS->whoseTurn]; i++) {
+			if (gS->hand[gS->whoseTurn][i] == treasure_map) {
+				actual++;
+			}
+		}
+	}
+
+	return assertTrue(actual, expected);
+}
+
 //int treasure_map_checkDeckCount() {
 //
 //}
