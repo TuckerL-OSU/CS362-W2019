7d8a0687b7443a6c072eadd162e871db1b10ca62
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-02-01 14:41:29 -0500

    testing ct4 t4,5,6

--- projects/lavellt/dominion/cardtest4.c
+++ projects/lavellt/dominion/cardtest4.c
@@ -5,16 +5,20 @@
 int treasure_map_checkNumCardsPlayed();
 int treasure_map_checkHand_worksOriginal();
 int treasure_map_checkHand_provesOriginalBug();
+int treasure_map_checkDeckCount();
+int treasure_map_checkCardsGained();
+int treasure_map_not2TM();
 
 int main() {
 	int passed = 0;
-	int numTests = 3;
+	int numTests = 6;
 
 	passed += treasure_map_checkNumCardsPlayed();
 	passed += treasure_map_checkHand_worksOriginal();
 	passed += treasure_map_checkHand_provesOriginalBug();
-	//passed += playCard_checkActionTest_treasure_map();
-	//passed += playCard_cardEffectTest();
+	passed += treasure_map_checkDeckCount();
+	passed += treasure_map_checkCardsGained();
+	passed += treasure_map_not2TM();
 
 	printf("%d/%d Treasure Map Card Tests Passed.\n", passed, numTests);
 }
@@ -48,9 +52,6 @@ int treasure_map_checkNumCardsPlayed() {
 int treasure_map_checkHand_worksOriginal() {
 	struct gameState* gS = newGame();
 
-	// can't check which cards are trashed, but we can set the 
-	// playedCardCount = 0, and if it stays at 0 when playing,
-	// treasure_map, then we can assume the treasure maps were discarded
 	int expected = 0;
 	int actual = 0;
 
@@ -81,9 +82,6 @@ int treasure_map_checkHand_worksOriginal() {
 int treasure_map_checkHand_provesOriginalBug() {
 	struct gameState* gS = newGame();
 
-	// can't check which cards are trashed, but we can set the 
-	// playedCardCount = 0, and if it stays at 0 when playing,
-	// treasure_map, then we can assume the treasure maps were discarded
 	int expected = 0;
 	int actual = 0;
 
@@ -110,14 +108,83 @@ int treasure_map_checkHand_provesOriginalBug() {
 }
 
 
-//int treasure_map_checkDeckCount() {
-//
-//}
-//
-//int treasure_map_checkCardsGained() {
-//
-//}
-//
-//int treasure_map_not2TM() {
-//
-//}
+int treasure_map_checkDeckCount() {
+	struct gameState* gS = newGame();
+
+	int expected = 5;
+	int actual = 0;
+
+	gS->whoseTurn = 0;
+
+	gS->handCount[gS->whoseTurn] = 4;
+	gS->hand[gS->whoseTurn][0] = treasure_map;
+	gS->hand[gS->whoseTurn][1] = copper;
+	gS->hand[gS->whoseTurn][2] = copper;
+	gS->hand[gS->whoseTurn][3] = treasure_map;
+
+	gS->deckCount[gS->whoseTurn] = 1;
+	gS->deck[gS->whoseTurn][0] = baron;
+
+	cardEffect(treasure_map, 0, 0, 0, gS, 0, 0);
+
+	// checking it is still P1
+	if (gS->whoseTurn == 0) {
+		actual = gS->deckCount[gS->whoseTurn]
+	}
+
+	return assertTrue(actual, expected);
+}
+
+int treasure_map_checkCardsGained() {
+	struct gameState* gS = newGame();
+
+	int expected = 0;
+	int actual = 0;
+
+	gS->whoseTurn = 0;
+
+	gS->handCount[gS->whoseTurn] = 4;
+	gS->hand[gS->whoseTurn][0] = treasure_map;
+	gS->hand[gS->whoseTurn][1] = copper;
+	gS->hand[gS->whoseTurn][2] = copper;
+	gS->hand[gS->whoseTurn][3] = treasure_map;
+
+	gS->deckCount[gS->whoseTurn] = 1;
+	gS->deck[gS->whoseTurn][0] = baron;
+
+	cardEffect(treasure_map, 0, 0, 0, gS, 0, 0);
+
+	// checking it is still P1, and checking to make sure deck is right size
+	if (gS->whoseTurn == 0 && gS->deckCount[gS->whoseTurn] == 5) {
+		int i;
+		// will check top 4 cards of deck to see if they are gold.
+		// if they are gold and the deck size is 5, it is safe to assume,
+		// baron was moved down in the deck correctly
+		for (i = 0; i < 4; i++) {
+			if (gS->deck[gS->whoseTurn][i] == gold) {
+				actual++;
+			}
+		}
+	}
+
+	return assertTrue(actual, expected);
+}
+
+int treasure_map_not2TM() {
+	struct gameState* gS = newGame();
+
+	int expected = 0;
+	int actual = -10;
+
+	gS->whoseTurn = 0;
+	gS->handCount[gS->whoseTurn] = 4;
+	gS->hand[gS->whoseTurn][0] = treasure_map;
+	gS->hand[gS->whoseTurn][1] = copper;
+	gS->hand[gS->whoseTurn][2] = copper;
+	gS->hand[gS->whoseTurn][3] = silver;
+	gS->playedCardCount = 0;
+
+	actual = cardEffect(treasure_map, 0, 0, 0, gS, 0, 0);
+
+	return assertTrue(actual, expected);
+}
