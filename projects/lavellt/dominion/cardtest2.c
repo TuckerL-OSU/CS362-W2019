2ab5b8e7a7f9a03105909f07184893c3e8ad0c2e
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-02-04 18:39:12 -0500

    added a 3rd test to ct2

--- projects/lavellt/dominion/cardtest2.c
+++ projects/lavellt/dominion/cardtest2.c
@@ -3,14 +3,16 @@
 #include "unittests.h"
 
 int smithy_numCardsDrawn();
-int smithy_numCardsDiscarded();
+int smithy_numCardsInHand();
+int smithy_smithyDiscarded();
 
 int main() {
 	int passed = 0;
-	int numTests = 2;
+	int numTests = 3;
 
 	passed += smithy_numCardsDrawn();
-	passed += smithy_numCardsDiscarded();
+	passed += smithy_numCardsInHand();
+	passed += smithy_smithyDiscarded();
 
 	printf("%d/%d Smithy Card Tests Passed.\n", passed, numTests);
 }
@@ -19,7 +21,7 @@ int main() {
 int smithy_numCardsDrawn() {
 	struct gameState* gS = newGame();
 
-	int expected = 3;
+	int expected = 2;
 	int actual = 0;
 
 	gS->whoseTurn = 0;
@@ -38,15 +40,15 @@ int smithy_numCardsDrawn() {
 	cardEffect(smithy, 0, 0, 0, gS, 0, 0);
 
 	if (gS->whoseTurn == 0) {
-		actual = gS->handCount[gS->whoseTurn];
+		actual = gS->deckCount[gS->whoseTurn];
 	}
 
-	printf("1. Check Number of Cards Drawn: ");
+	printf("1. Check Number of Cards in Deck: ");
 	return assertTrue(actual, expected);
 }
 
 // check to make sure smithy was discarded afterwards
-int smithy_numCardsDiscarded() {
+int smithy_numCardsInHand() {
 	struct gameState* gS = newGame();
 
 	int expected = 3;
@@ -71,6 +73,37 @@ int smithy_numCardsDiscarded() {
 		actual = gS->handCount[gS->whoseTurn];
 	}
 
-	printf("2. Check Number of Cards Discarded: ");
+	printf("2. Check Number of Cards in hand: ");
+	return assertTrue(actual, expected);
+}
+
+int smithy_smithyDiscarded() {
+	struct gameState* gS = newGame();
+
+	int expected = smithy;
+	int actual = 0;
+
+	gS->whoseTurn = 0;
+
+	// set hand
+	gS->handCount[gS->whoseTurn] = 1;
+	gS->hand[gS->whoseTurn][0] = smithy;
+	// "load" deck
+	gS->deckCount[gS->whoseTurn] = 5;
+	gS->deck[gS->whoseTurn][0] = copper;
+	gS->deck[gS->whoseTurn][1] = copper;
+	gS->deck[gS->whoseTurn][2] = copper;
+	gS->deck[gS->whoseTurn][3] = silver;
+	gS->deck[gS->whoseTurn][4] = silver;
+
+	gS->playedCardCount = 0;
+
+	cardEffect(smithy, 0, 0, 0, gS, 0, 0);
+
+	if (gS->whoseTurn == 0) {
+		actual = gS->playedCards[gS->playedCardCount];
+	}
+
+	printf("3. Check Discard for Smithy: ");
 	return assertTrue(actual, expected);
 }
