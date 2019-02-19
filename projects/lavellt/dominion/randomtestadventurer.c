aaa0276e500be3ae2ed0038e2cf9f358031b46f4
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-02-19 15:48:06 -0500

    .

--- projects/lavellt/dominion/randomtestadventurer.c
+++ projects/lavellt/dominion/randomtestadventurer.c
@@ -42,15 +42,15 @@ int main() {
 		// set up a game
 		initializeGame(numPlayers, k, seed, &pre);
 		// set a players turn, which player doesn't matter
-		pre->whoseTurn = 0;
-		currentPlayer = pre->whoseTurn;
+		pre.whoseTurn = 0;
+		currentPlayer = pre.whoseTurn;
 		deckSize = rand() % (MAX_DECK + 1);
 		// just to prevent the hand from being bigger than the deck
 		handSize = rand() % (deckSize + 1);
 
 		// number of cards left in deck is total - number in hand
-		pre->deckCount[0] = deckSize - handSize;
-		pre->handCount[0] = handSize;
+		pre.deckCount[0] = deckSize - handSize;
+		pre.handCount[0] = handSize;
 
 		// position of adventurer
 		int handPos = rand() % handSize;
@@ -59,78 +59,78 @@ int main() {
 		//for (j = 0; j < numPlayers; j++) {
 		
 			int j;
-			for (j = 0; j < pre->deckCount[currentPlayer]; j++) {
+			for (j = 0; j < pre.deckCount[currentPlayer]; j++) {
 				int randomCard = rand() % (26 + 0);
 				// attempt to stack the deck with some treasures just to be safe
 				if (randomCard == copper) {
-					pre->deck[currentPlayer][j] = copper;
+					pre.deck[currentPlayer][j] = copper;
 				}
 				else if (randomCard == silver) {
-					pre->deck[currentPlayer][j] = silver;
+					pre.deck[currentPlayer][j] = silver;
 				}
 				else if (randomCard == gold) {
-					pre->deck[currentPlayer][j] = gold;
+					pre.deck[currentPlayer][j] = gold;
 				}
 				else {
 					// else fill hand with a random kingdom card
 					int randomK = rand() % (10);
-					pre->deck[currentPlayer][j] = k[randomK];
+					pre.deck[currentPlayer][j] = k[randomK];
 				}
 			}
 		//}
 
-		//for (m = 0; m < pre->handCount[curPlayer]; m++) {
-		//	if (pre->hand[curPlayer][m] == copper || pre->hand[curPlayer][m] == silver || pre->hand[curPlayer][m] == gold) {
+		//for (m = 0; m < pre.handCount[curPlayer]; m++) {
+		//	if (pre.hand[curPlayer][m] == copper || pre.hand[curPlayer][m] == silver || pre.hand[curPlayer][m] == gold) {
 		//		coinCountBefore++;
 		//	}
 		//}
 
 		// coins is unused so I am going to use it here for testing
-		pre->coins = countNumTreasureCards(pre->whoseTurn, &pre);
+		pre.coins = countNumTreasureCards(pre.whoseTurn, &pre);
 
 		// create the object to do work on
 		struct gameState post;
 		memcpy(&post, pre, sizeof(struct gameState));
 		
 		//printf("Coin Count before: %d\n", coinCount);
-		//printf("discard count before: %d\n", pre->discardCount[curPlayer]);
-		//printf("deckSize: %d, deck: %d, and hand: %d\n", deckSize, pre->deckCount[0], pre->handCount[0]); 
+		//printf("discard count before: %d\n", pre.discardCount[curPlayer]);
+		//printf("deckSize: %d, deck: %d, and hand: %d\n", deckSize, pre.deckCount[0], pre.handCount[0]); 
 		cardEffect(adventurer, 0, 0, 0, &post, handPos, 0);
 
 		coinCount = 0;
 
-		//for (m = 0; m < pre->handCount[curPlayer]; m++) {
-		//	if (pre->hand[curPlayer][m] == copper || pre->hand[curPlayer][m] == silver || pre->hand[curPlayer][m] == gold) {
+		//for (m = 0; m < pre.handCount[curPlayer]; m++) {
+		//	if (pre.hand[curPlayer][m] == copper || pre.hand[curPlayer][m] == silver || pre.hand[curPlayer][m] == gold) {
 		//		coinCount++;
 		//	}
 		//}
 
-		post->coins += countNumTreasureCards(post->whoseTurn, &post);
+		post.coins += countNumTreasureCards(post.whoseTurn, &post);
 
 		//printf("Coin Count after: %d\n", coinCount);
-		//printf("discard count after: %d\n", pre->discardCount[curPlayer]);
+		//printf("discard count after: %d\n", pre.discardCount[curPlayer]);
 		discardCopper = 0;
 		discardSilver = 0;
 		discardGold = 0;
-		for (x = 0; x < post->discardCount[curPlayer]; x++) {
-			if (post->discard[curPlayer][x] == copper) {
+		for (x = 0; x < post.discardCount[curPlayer]; x++) {
+			if (post.discard[curPlayer][x] == copper) {
 				discardCopper++;
 			}
-			else if (post->discard[curPlayer][x] == silver) {
+			else if (post.discard[curPlayer][x] == silver) {
 				discardSilver++;
 			}
-			else if (post->discard[curPlayer][x] == gold) {
+			else if (post.discard[curPlayer][x] == gold) {
 				discardGold++;
 			}
 		}
 		int passed = 1;
-		if (post->coins > (pre->coins + 2)) {
+		if (post.coins > (pre.coins + 2)) {
 			printf("Too many cards drawn: Test Failed\n\n");
 			drawTestFailed++;
 			passed = 0;
 		}
 
-		if (post->coins < pre->coins) {
+		if (post.coins < pre.coins) {
 			printf("Fewer cards exist in hand than were first present: Test Failed\n\n");
 			drawTestFailed++;
 			passed = 0;
